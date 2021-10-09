e78a0d6bda0c155e3f9b054fc4921b22011e8fae
jpceia <joao.p.ceia@gmail.com>
2021-10-09 07:47:39 +0100

    added unistd.h include for usleep

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "philosophers.h"
+#include <unistd.h>
 #include <sys/time.h>
 #include <stdlib.h>
 #include <stdio.h>
